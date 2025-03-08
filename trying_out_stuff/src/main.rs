5e078d10ad6ae2405daf8fe25226fd7110c3720f
Xevelence <andi.fritz.2004@gmail.com>
2025-03-08 22:50:18 +0100

    tried out some stuff with structs

--- trying_out_stuff/src/main.rs
+++ trying_out_stuff/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-	println!("{}", u8::MAX);
+	#[derive(Debug)]
+	struct Cuboid {
+		width: u32,
+		height: u32,
+		depth: u32
+	}
+	impl Cuboid {
+		fn area(&self) -> u32 {
+			self.width * self.height * self.depth
+		}
+		fn new(width: u32, height: u32, depth: u32) -> Cuboid {
+			Cuboid { width, height, depth }
+		}
+	}
+	let cub = Cuboid::new(10, 10, 10);
+	println!("{}", cub.area());
+	println!("cub has the following state: {:#?}", cub)
 }
