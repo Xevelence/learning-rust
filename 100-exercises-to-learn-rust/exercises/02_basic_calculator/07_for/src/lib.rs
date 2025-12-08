fbafbcee86e2f693a7f29b86e7c162ff1598aea1
Xevelence <andi.fritz.2004@gmail.com>
2025-12-08 15:45:30 +0100

    reset progress for 100-exercises-to-learn-rust

--- 100-exercises-to-learn-rust/exercises/02_basic_calculator/07_for/src/lib.rs
+++ 100-exercises-to-learn-rust/exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,13 +1,6 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    let mut result: u32 = 1;
-    if n <= 1 {return 1;}
-    else {
-        for i in 2..=n {
-            result *= i;
-        }
-    }
-    result
+    todo!()
 }
 
 #[cfg(test)]
